7780703d98d57cff690636f588ecfd786f2f00b8
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2018-12-23 02:54:46 +0200

    Adds random position of present and handle cristmas tree controls

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
   const scoreBoard = document.createElement("div");
   const present = "🎁";
   const interval = 10;
+  const appPaddingVertical = 100;
+  // using keyCode instead of key, as in other languages symbol might differ
+  const upKeyCodes = [87, 38]; // w & arrow up
+  const downKeyCodes = [83, 40]; // s & arrow down
+  let controlsInterval = -1;
   let score = 0;
   let allPresents = [];
   let time = 0;
@@ -16,6 +21,7 @@
 
   app.appendChild(christmasTree);
   app.appendChild(scoreBoard);
+  app.style.padding = `${appPaddingVertical}px 0`;
 
   const checkCollision = currentPresent => {
     const horizontalCollision =
@@ -33,6 +39,28 @@
     return horizontalCollision && verticalCollision;
   };
 
+  const move = direction => {
+    const currentPosition = christmasTree.offsetTop;
+    let newPosition = currentPosition;
+    if (direction === "up") {
+      newPosition = newPosition - 1;
+    }
+    if (direction === "down") {
+      newPosition = newPosition + 1;
+    }
+
+    christmasTree.style.top = `${newPosition}px`;
+  };
+
+  const handleControls = ({ keyCode }) => {
+    let direction = null;
+    direction = upKeyCodes.includes(keyCode) ? "up" : direction;
+    direction = downKeyCodes.includes(keyCode) ? "down" : direction;
+    if (direction && controlsInterval === -1) {
+      controlsInterval = setInterval(() => move(direction), 5);
+    }
+  };
+
   const render = () => {
     allPresents.forEach(currentPresent => {
       if (checkCollision(currentPresent)) {
@@ -44,14 +72,26 @@
     scoreBoard.textContent = score;
   };
 
+  const randomPosition = () => {
+    const { floor, random } = Math;
+    const screenHeight = app.offsetHeight - 2 * appPaddingVertical;
+    const topOffset = floor(random() * screenHeight) + appPaddingVertical;
+
+    return topOffset;
+  };
+
   const startGame = () => {
     intervalId = setInterval(() => {
       time += interval;
 
       if (time % 1000 === 0) {
         const newPresent = document.createElement("div");
+        const topOffset = randomPosition();
+
         newPresent.className = "Present";
         newPresent.textContent = present;
+        newPresent.style.top = `${topOffset}px`;
+
         app.appendChild(newPresent);
         allPresents = [...allPresents, newPresent];
       }
@@ -62,4 +102,10 @@
 
   render();
   window.addEventListener("load", startGame);
+  document.addEventListener("keydown", handleControls);
+  document.addEventListener("keyup", () => {
+    console.log();
+    clearInterval(controlsInterval);
+    controlsInterval = -1;
+  });
 })();
